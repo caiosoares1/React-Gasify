f1ef9830ac7c34822d06ba269b2c6aefcb2d7e16
caiosoares1 <batistacaio2244@gmail.com>
2023-11-29 12:38:35 -0300

    Iniciando repositório com react

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,100 @@
-import Image from 'next/image'
-
+import Gasify from '@/components/images'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="./images/logoLS-no-bg.svg" type="image/x-icon" />
+
+        <title>Gasify</title>
+      </head>
+
+      <body className="bg-[--gasify-branco] min-h-[100vh]">
+        <header className="bg-white content-center max-w-[100vw] flex rounded-3xl justify-between mx-12 mt-12 mb-6 p-4 shadow-md">
+          <div id="gasify-logo" className="flex content-center items-center space-x-2">
+            <img src="./images/logoLS-no-bg.svg" alt="Gasify logo" className="" />
+            <Gasify />
+          </div>
+          <div className="flex justify-between items-center">
+            <img src="./images/logo_posto.svg" alt="Logo posto canaã" />
+            <p className="text-[--gasify-preto-claro] ml-8 hidden lg:flex lg:flex-col">
+              Rede de Postos de Combustível Canaã
+            </p>
+          </div>
+          <div className="flex justify-between items-center">
+            <img className="max-w-10 max-h-10 rounded-full m-2" id="user" src="./images/FrentistaLC.jpg" alt="User icon" />
+            <div id="username" className="hidden lg:flex lg:flex-col">
+              <p className="text-[--gasify-preto-claro] text-center">Bem-vindo</p>
+              <span className="font-semibold">Luiz Carlos!</span>
+            </div>
+            <img id="arrow" src="./images/icons/logout.svg" alt="Arrow icon" className="ml-12 w-7" />
+          </div>
+        </header>
+
+        <div id="compatibility" className="md:hidden flex flex-col justify-center items-center text-center h-[70vh] my-auto">
+          <img src="./images/compatibility.png" alt="Compatibility icon" className="w-40 h-40" />
+          <p className="text-[--gasify-preto-claro] ml-2 font-bold font-2xl">
+            Seu dispositivo não é compatível com o Gasify
+          </p>
+          <div className="flex justify-center items-center">
+            <p className="text-[--gasify-preto-claro]">
+              Por favor, acesse o Gasify em um dispositivo com tela maior
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <nav className="justify-around space-x-8 mx-4 my-14 text-center hidden md:flex">
+          <p className="w-[100%] font-medium border-[--gasify-preto] border-b-2 cursor-pointer text-[--gasify-preto] transition-all hover:border-b-[--gasifty-cinza] hover:text-[--gasify-cinza]"
+            id="nav-escolha-produto">
+            Abastecimento
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p className="w-[100%] font-medium border-[--gasify-preto-claro] border-b-2 cursor-pointer text-[--gasify-cinza-claro] transition-all hover:border-b-[--gasifty-cinza] hover:text-[--gasify-cinza]"
+            id="nav-uso-cc">
+            Use seu Carbon Cash para resgatar!
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+          <p className="w-[100%] font-medium border-[--gasify-preto-claro] border-b-2 cursor-pointer text-[--gasify-cinza-claro] transition-all hover:border-b-[--gasifty-cinza] hover:text-[--gasify-cinza]"
+            id="nav-pagamento">
+            Pagamento
           </p>
-        </a>
+        </nav>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <main className="w-[85%] mx-auto mt-12 hidden md:flex md:flex-col">
+          <div id="saldo-cards" className="flex justify-around space-x-8 mx-4 mt-8">
+            <div className="shadow-md w-[50%] p-4 space-y-2 rounded-lg flex flex-col justify-around">
+              <p className="text-[--gasify-verde] font-bold text-center text-4xl" id="total">
+                R$ 0,00
+              </p>
+              <p className="text-[--gasify-preto-claro] text-center text-xl font-semibold">
+                Valor a pagar
+              </p>
+            </div>
+
+            <div className="shadow-md w-[50%] p-4 space-y-2 rounded-lg">
+              <p className="flex justify-center items-center text-[--gasify-verde] font-bold text-center text-4xl" id="total-cc">
+                <img src="./images/logoLS-no-bg.svg" className="mr-2" alt="Carbon Cash icon" /><span id="total-cc-text">0
+                  CC</span>
+              </p>
+              <p className="text-[--gasify-preto-claro] text-center text-xl font-semibold">
+                Carbon Cash a receber
+              </p>
+            </div>
+          </div>
+          <form>
+            <div id="card-combustiveis" className="flex flex-wrap justify-between mx-auto my-12 gap-y-12 gap-x-12"></div>
+
+            <button type="submit"
+              className="w-full transition-all bg-[--gasify-verde-claro] mt-8 rounded-lg text-white font-semibold h-16 hover:bg-[--gasify-verde]">
+              Ir ao pagamento
+            </button>
+          </form>
+        </main>
+        <footer className="">
+          <img className="w-[100%]" src="./images/footer.png" alt="" />
+        </footer>
+        <script type="module" src="./src/main.js"></script>
+        <script type="module" src="./src/atendimento.js"></script>
+      </body>
+    </>
   )
 }
---
+++ src/components/images.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Gasify() {
+    return (
+        <img
+          src="/images/gato.jpg"
+          className="hidden lg:flex"
+          alt="Picture of the author"
+        />
+    )
+}
