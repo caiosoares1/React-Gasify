a59065b11fcff66c670d35bdd09a3fa331e302b1
caiosoares1 <batistacaio2244@gmail.com>
2023-11-29 22:02:07 -0300

    Adicionando componentes das imagens na página dos Cards

--- src/components/images.js
+++ src/components/images.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+
+
 export function Gasify() {
     return (
         <img
@@ -13,10 +15,61 @@ export function Gasify() {
 export function GasifyLogo() {
     return (
         <img
-          src="/images/gasify-logo.svg"
+          src="/images/logoLS-no-bg.svg"
           className="hidden lg:flex"
           alt="Picture of the author"
         />
     )
 }
 
+export function LogoPosto() {
+    return (
+        <img
+          src="/images/logo_posto.svg"
+          className="hidden lg:flex"
+          alt="Picture of the author"
+        />
+    )
+}
+
+export function User() {
+    return (
+        <img
+          src="/images/FrentistaLC.jpg"
+          id="user"
+          className="max-w-10 max-h-10 rounded-full m-2"
+          alt="User icon"
+        />
+    )
+}
+
+export function Arrow() {
+    return (
+        <img
+          src="/images/icons/logout.svg"
+          id="arrow"
+          alt="Arrow icon"
+          className="ml-12 w-7"
+        />
+    )
+}
+
+export function Compatibility() {
+    return (
+        <img
+          src="/images/compatibility.png"
+          alt="Compatibility icon"
+          className="w-40 h-40"
+        />
+    )
+}
+
+export function Footer() {
+    return (
+        <img
+          src="/images/footer.png"
+          alt="Footer icon"
+          className="w-[100vw]"
+        />
+    )
+}
