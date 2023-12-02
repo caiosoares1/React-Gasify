596c3f6c892492379bc1fe551990614a8b3f470d
caiosoares1 <batistacaio2244@gmail.com>
2023-12-02 15:05:27 -0300

    Adicionando variáveis de ambiente para KEY e URL

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
  
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qa3VpZXZ1ZnJlZWZqd252dW9zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5OTI5MzU5NywiZXhwIjoyMDE0ODY5NTk3fQ.XLifW8hovrcSmar-NZzTg1zUV9i6OBa5PDBazlPExrQ';
-const API_URL = 'https://mjkuievufreefjwnvuos.supabase.co' + '/rest/v1';
+const API_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const API_URL = process.env.NEXT_PUBLIC_SUPABASE_URL + '/rest/v1';
  
 export const supabase = axios.create({
   baseURL: API_URL,
