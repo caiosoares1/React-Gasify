cad9442e0bfc017afd41b2b2433c8dbe1665e993
Caio Soares <101191810+caiosoares1@users.noreply.github.com>
2023-12-06 13:50:09 -0300

    Update abastecimento.js

--- src/pages/abastecimento.js
+++ src/pages/abastecimento.js
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react'
 import * as image from '@/components/images'
 import CompatibilityComponent from '@/components/compatibility';
+import Header from '@/components/header';
 import CardProduto from '@/components/cardProduto'
 // import { products } from '@/data/products'
 // import { supabase } from '@/services/supabase'
@@ -37,26 +38,7 @@ export default function Abastecimento() {
     <>
       <div className="bg-[--gasify-branco] min-h-[100vh]">
         {/* Transformar em componente */}
-        <header className="bg-white content-center max-w-[100vw] flex rounded-3xl justify-between mx-12 mt-12 mb-6 p-4 shadow-md">
-          <div id="gasify-logo" className="flex content-center items-center space-x-2">
-            <image.GasifyLogo />
-            <image.Gasify />
-          </div>
-          <div className="flex justify-between items-center">
-            <image.LogoPosto />
-            <p className="text-[--gasify-preto-claro] ml-8 hidden lg:flex lg:flex-col">
-              Rede de Postos de Combustível Canaã
-            </p>
-          </div>
-          <div className="flex justify-between items-center">
-            <image.User />
-            <div id="username" className="hidden lg:flex lg:flex-col">
-              <p className="text-[--gasify-preto-claro] text-center">Bem-vindo</p>
-              <span className="font-semibold">Luiz Carlos!</span>
-            </div>
-            <image.Arrow />
-          </div>
-        </header>
+        <Header />
 
         {/* Transformar em componente */}
         <CompatibilityComponent />
