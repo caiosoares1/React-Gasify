a8f56db0fc33b47586a038f17ce66f08b08c350e
caiosoares1 <batistacaio2244@gmail.com>
2023-12-18 00:13:41 -0300

    ajustes de sintaxe

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -17,9 +17,8 @@ export default function RootLayout({ children }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        {/* {metadata.title && <title>{metadata.title}</title>} */}
       </head>
-      <body className="poppins">
+      <body >
         <AbastecimentoProvider>
           {children}
         </AbastecimentoProvider>
--- src/components/loginComponent.jsx
+++ src/components/loginComponent.jsx
@@ -25,9 +25,9 @@ export default function LoginComponent() {
     }
     return (
         <cfg className="h-[100vh] w-[100vw] bg-[--gasify-fundo] overscroll-none overflow-hidden">
-        <header class="h-[10vh] w-[100vw] relative">
-            <div class="flex justify-center md:justify-between items-start mx-auto mt-[75px] md:ml-[75px]">
-                <div class="w-[250px] lg:w-auto flex justify-around  gap-x-5">
+        <header className="h-[10vh] w-[100vw] relative">
+            <div className="flex justify-center md:justify-between items-start mx-auto mt-[75px] md:ml-[75px]">
+                <div className="w-[250px] lg:w-auto flex justify-around  gap-x-5">
                     <image.GasifyLogo className="w-[5rem] 2xl:w-[9rem]"/>
                     <image.Gasify className="w-[10rem] 2xl:w-[14rem]"/>
                     <link rel="icon" href="./images/logoLS-no-bg.svg" type="image/x-icon" />
@@ -35,36 +35,36 @@ export default function LoginComponent() {
                 <image.FolhasVerdesCantoSuperior/>
             </div>
         </header>
-        <div id="compatibility" class="md:hidden flex flex-col justify-center items-center text-center h-[70vh] my-auto">
+        <div id="compatibility" className="md:hidden flex flex-col justify-center items-center text-center h-[70vh] my-auto">
             <image.Compatibility/>
-            <p class="text-[--gasify-preto-claro] ml-2 font-bold font-2xl">
+            <p className="text-[--gasify-preto-claro] ml-2 font-bold font-2xl">
                 Seu dispositivo não é compatível com o Gasify
             </p>
-            <div class="flex justify-center items-center">
-                <p class="text-[--gasify-preto-claro]">
+            <div className="flex justify-center items-center">
+                <p className="text-[--gasify-preto-claro]">
                     Por favor, acesse o Gasify em um dispositivo com tela maior
                 </p>
             </div>
         </div>
-        <main class="hidden md:flex justify-around h-[90vh] w-[100vw] items-center">
-            <div class="flex flex-col justify-center items-center gap-y-[25px] w-auto h-auto self-start mt-10 2xl:mt-28 mb-[50] ">
+        <main className="hidden md:flex justify-around h-[90vh] w-[100vw] items-center">
+            <div className="flex flex-col justify-center items-center gap-y-[25px] w-auto h-auto self-start mt-10 2xl:mt-28 mb-[50] ">
                 <image.GarotoLogin/>
-                    <h1 class="text-neutral-900 md:text-2xl 2xl:text-5xl font-semibold font-['Poppins'] gap-y-8 xl:text-4xl">Acesse sua conta!</h1>
-                    <form id="form" onSubmit={handleLogin} class="bg-white rounded-[20px] shadow-2xl 2xl:w-auto 2xl:h-auto xl:w-[auto] xl:h-[auto] z-10">
-                        <div class="flex flex-col p-10">
-                            <div class="flex flex-col justify-center mt-5 gap-y-1">
-                                <label for="email" class="text-gray-800 text-xl font-semibold font-['Poppins']">Email</label>
-                                <input placeholder="fulano@email.com" type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} class="border-2 border-black rounded-md p-6 xl:w-[500px] 2xl:w-[700px] h-[50px]" />
+                    <h1 className="text-neutral-900 md:text-2xl 2xl:text-5xl font-semibold font-['Poppins'] gap-y-8 xl:text-4xl">Acesse sua conta!</h1>
+                    <form id="form" onSubmit={handleLogin} className="bg-white rounded-[20px] shadow-2xl 2xl:w-auto 2xl:h-auto xl:w-[auto] xl:h-[auto] z-10">
+                        <div className="flex flex-col p-10">
+                            <div className="flex flex-col justify-center mt-5 gap-y-1">
+                                <label htmlFor="email" className="text-gray-800 text-xl font-semibold font-['Poppins']">Email</label>
+                                <input placeholder="fulano@email.com" type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="border-2 border-black rounded-md p-6 xl:w-[500px] 2xl:w-[700px] h-[50px]" />
                             </div>
-                            <div class="flex flex-col justify-center mt-5 content-start gap-y-1">
-                                <label for="password" class="text-gray-800 text-xl font-semibold font-['Poppins']">Senha</label>
-                                <input placeholder="12345678" type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} class="border-2 border-black rounded-md p-6 xl:w-[500px] 2xl:w-[700px] h-[50px]" />
+                            <div className="flex flex-col justify-center mt-5 content-start gap-y-1">
+                                <label htmlFor="password" className="text-gray-800 text-xl font-semibold font-['Poppins']">Senha</label>
+                                <input placeholder="12345678" type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="border-2 border-black rounded-md p-6 xl:w-[500px] 2xl:w-[700px] h-[50px]" />
                             </div>
-                            <div class="flex justify-end">
-                                <a class=" text-zinc-700 text-base font-normal font-['Poppins'] p-6 pr-0 transition hover-[text-]" href="">Esqueci a senha</a>
+                            <div className="flex justify-end">
+                                <a className=" text-zinc-700 text-base font-normal font-['Poppins'] p-6 pr-0 transition hover-[text-]" href="">Esqueci a senha</a>
                             </div>
-                            <div class="flex flex-col justify-center items-center mt-5 mb-4">
-                                <button type="submit" class="w-[251px] h-[52px] rounded-lg bg-[--gasify-verde-claro] text-white text-xl font-semibold font-['Poppins'] transition hover:bg-[--gasify-verde]">Login</button>
+                            <div className="flex flex-col justify-center items-center mt-5 mb-4">
+                                <button type="submit" className="w-[251px] h-[52px] rounded-lg bg-[--gasify-verde-claro] text-white text-xl font-semibold font-['Poppins'] transition hover:bg-[--gasify-verde]">Login</button>
                             </div>
                         </div>
                     </form>
